772d99081d62bb207f3030b4042719891afb2938
Gregory McCormack <gregorym2802@tuckahoeschools.org>
2024-11-06 13:32:55 +0000

    inital commit

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 //Use query Selectors to get the class of all the button shapes
-
+const redLight = document.querySelector('.red-light');
+const greenLight = document.querySelector('.green-light');
+const yellowLight = document.querySelector('.yellow-light');
+const onLight = document.querySelector('.on-light');
+const offLight = document.querySelector('.off-light');
+const partyLight = document.querySelector('.party-light');
 //Use getElementById which allows us to get the ID so that we can add an event listener to the buttons, listen for a click and then set the color via css properties.
-
+const redSwitch = document.getElementById("red-switch");
+const greenSwitch = document.getElementById("green-switch");
+const yellowSwitch = document.getElementById("yellow-switch");
+const onSwitch = document.getElementById("on-switch");
+const offSwitch = document.getElementById("off-switch");
+const partySwitch = document.getElementById("party-switch");
 //Add event listeners that listen for a button click
+redSwitch.addEventListener("click", () => {
+    redLight.style.backgroundColor = "red";
+});
+greenSwitch.addEventListener("click", () => {
+    greenLight.style.backgroundColor = "green";
+});
+yellowSwitch.addEventListener("click", () => {
+    yellowLight.style.backgroundColor = "yellow";
+});
 
 //Turn all lights on and off with their specific buttons
+offSwitch.addEventListener("click", () => {
+    redLight.style.backgroundColor = "grey";
+    greenLight.style.backgroundColor = "grey";
+    yellowLight.style.backgroundColor = "grey";
+});
+onSwitch.addEventListener("click", () => {
+    redLight.style.backgroundColor = "red";
+    greenLight.style.backgroundColor = "green";
+    yellowLight.style.backgroundColor = "yellow";
+});
 
 //Party Mode Button
 
